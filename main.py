a6c7baee318debb1b53b46804ff59619046c588a
Sushan2589 <85663813+Sushan2589@users.noreply.github.com>
2024-04-26 13:09:15 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ print ("Your Local Time currently is:",Time)
 Hour=int(time.strftime("%H"))
 
 
-if Hour==6 and Hour>=7 and Hour<12:
+if Hour<=6 and Hour<12:
   print("Good Morning!!")
 
-elif Hour>=12 and Hour<=17:
+elif Hour>=12 and Hour<18:
  print("Good Afternoon!!")
 
-elif Hour>=18 and Hour<=24:
+elif Hour>=18 and Hour<23:
   print("Good Evening, time to go to bed ")
 
 else:
